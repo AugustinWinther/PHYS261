3c8acae02b736fb8509ca2976b00fa72b57d062f
AugustinWinther <augustin@winther.io>
2025-09-02 22:25:38 +0200

    exercise_2: Refactor

--- exercise_2/calc.py
+++ exercise_2/calc.py
@@ -21,22 +21,21 @@ def R_values(r_max: float, prec: int,
     r_max and the corresponding r (distance) values.
     
     """
+    # Last k-index in bk series with non-zero value. Series is zero after that
     k_max = n - L - 1
 
     # Create column vector with all r values used in integration
-    dr = r_max/prec
-    r = np.arange(0, r_max + dr, dr)[:, np.newaxis]
+    r = np.linspace(0.0, r_max, prec)[:, np.newaxis]
 
-    # Row vector containing all k-values
-    k = (np.arange(0, k_max + 1, dtype=np.float64))[np.newaxis, :]
+    # Row vector containing all integer k-values from 0 to k_max
+    k = (np.arange(0.0, k_max + 1.0,))[np.newaxis, :]
 
     # Recursively calculate bk values if it has more than 1 value (b0)
     bk = np.empty_like(k)
     bk[0, 0] = _b0_value(n, L, Z)
 
-    if k_max > 0:
-        for j in range(k_max):  # Don't use k as iterator; ruins k vector
-            bk[0, j + 1] = (2*(j + L - n + 1)/((j + 1)*(j + 2*L + 2)))*bk[0, j]
+    for j in range(k_max):  # Don't use k as iterator; ruins k vector
+        bk[0, j + 1] = (2*(j + L - n + 1)/((j + 1)*(j + 2*L + 2)))*bk[0, j]
 
     # Calculate the sums in R(r)
     R_sums = np.sum(bk*((Z*r)/(n))**(k + L), axis=1)
